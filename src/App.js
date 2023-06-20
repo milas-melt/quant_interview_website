358b6f9304f897482d740ae4ae06a2116cd1cb2b
milas-melt <tlemcani.salim00@gmail.com>
2023-06-20 20:28:23 +0100

    bug fix

--- src/App.js
+++ src/App.js
@@ -146,7 +146,7 @@ export default function App() {
                                 hint ? "visible" : "invisible"
                             } rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600`}
                         >
-                            {showSolution ? "Hide" : "Show"} Hint
+                            {showHint ? "Hide" : "Show"} Hint
                         </button>
 
                         <HintModal
